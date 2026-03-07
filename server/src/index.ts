97f9bd5af19df1de1634a247745d81d0ab914a9f
Antony Figueroa <antonysamuel0903@gmail.com>
2026-03-07 19:43:56 -0400

    fix: add Vercel URL to CORS allowed origins

--- server/src/index.ts
+++ server/src/index.ts
@@ -55,7 +55,9 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://127.0.0.1:5174',
   'http://localhost:3000',
-  'http://127.0.0.1:3000'
+  'http://127.0.0.1:3000',
+  'https://ecommerce-eosin.vercel.app',
+  config.frontendUrl
 ];
 
 app.use((req, res, next) => {
